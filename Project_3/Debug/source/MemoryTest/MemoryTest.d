78faf6b4493bb8623aed7832a43c9178d8ca102e
John Taylor <john.t.taylor@puregas.com>
2019-10-14 15:33:45 -0600

    adding necessary build files and folders

---
+++ Project_3/Debug/source/MemoryTest/MemoryTest.d
@@ -0,0 +1,7 @@
+source/MemoryTest/MemoryTest.o source/MemoryTest/MemoryTest.d: \
+ ../source/MemoryTest/MemoryTest.c ../source/MemoryTest/MemoryTest.h \
+ ../source/MemoryTest/../Pattern/patternGenerator.h
+
+../source/MemoryTest/MemoryTest.h:
+
+../source/MemoryTest/../Pattern/patternGenerator.h:
